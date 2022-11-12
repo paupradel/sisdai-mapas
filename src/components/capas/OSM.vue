791cd5a63dc884ee5304c1d9a172443e5a16489a
andres-geotec <andres.geotec@gmail.com>
2022-11-12 16:58:28 -0600

    composable de capa tipo mosaico

--- src/components/capas/OSM.vue
+++ src/components/capas/OSM.vue
@@ -1,30 +1,22 @@
 <script>
 import TileLayer from 'ol/layer/Tile'
 import OSM from 'ol/source/OSM'
-import TileEventType from 'ol/source/TileEventType'
-
-import usarCapa, { props, emits } from '../../composables/usarCapa'
+import usarCapaMosaico, {
+  props,
+  emits,
+} from './../../composables/usarCapaMosaico'
 
 export default {
   name: 'SisdaiCapaXyzOsm',
   props,
-  emits: ['alIniciarCargaMosaico', 'alFinalizarCargaMosaico', ...emits],
+  emits,
   setup(propsSetup, { emit }) {
-    const source = new OSM()
-
-    source.on(TileEventType.TILELOADSTART, () => emit('alIniciarCargaMosaico'))
-    source.on([TileEventType.TILELOADEND, TileEventType.TILELOADERROR], e => {
-      emit('alFinalizarCargaMosaico', e.type === TileEventType.TILELOADEND)
-    })
-
-    usarCapa(propsSetup, emit).registrar(
+    usarCapaMosaico(propsSetup, emit).registrar(
       new TileLayer({
-        source,
+        source: new OSM(),
         // className: this.className,
       })
     )
-
-    return {}
   },
   render: () => null,
 }
--- src/components/capas/XYZ.vue
+++ src/components/capas/XYZ.vue
@@ -1,32 +1,20 @@
 <script>
 import TileLayer from 'ol/layer/Tile'
 import XYZ from 'ol/source/XYZ'
-import TileEventType from 'ol/source/TileEventType'
 
-import usarCapa, { props, emits } from '../../composables/usarCapa'
+import usarCapaMosaico, {
+  props,
+  emits,
+} from './../../composables/usarCapaMosaico'
 
 export default {
   name: 'SisdaiCapaXyz',
-  props: {
-    url: {
-      type: String,
-      default:
-        'https://{a-c}.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}.png',
-    },
-    ...props,
-  },
-  emits: ['alIniciarCargaMosaico', 'alFinalizarCargaMosaico', ...emits],
+  props,
+  emits,
   setup(propsSetup, { emit }) {
-    const source = new XYZ({ url: propsSetup.url, crossOrigin: 'Anonymous' })
-
-    source.on(TileEventType.TILELOADSTART, () => emit('alIniciarCargaMosaico'))
-    source.on([TileEventType.TILELOADEND, TileEventType.TILELOADERROR], e => {
-      emit('alFinalizarCargaMosaico', e.type === TileEventType.TILELOADEND)
-    })
-
-    usarCapa(propsSetup, emit).registrar(
+    usarCapaMosaico(propsSetup, emit).registrar(
       new TileLayer({
-        source,
+        source: new XYZ({ url: propsSetup.url, crossOrigin: 'Anonymous' }),
         // className: this.className,
       })
     )
