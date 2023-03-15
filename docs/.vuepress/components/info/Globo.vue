4f39bd686355806950fb16cffac6506b229d17e3
andres-geotec <andres.geotec@gmail.com>
2023-03-15 16:03:56 -0600

    documentación del globo informativo

--- docs/.vuepress/components/info/Globo.vue
+++ docs/.vuepress/components/info/Globo.vue
@@ -1,15 +1,24 @@
 <script setup>
 import edos from './../../capas/ejemplo-edos.json'
+// import centEdos from './../../capas/centroides-estados.json'
+
+const contenidoGloboInformativo = feature => `Estdo: <b>${feature.nom_edo}</b>`
 </script>
 
 <template>
   <SisdaiMapa :vista="{ centro: [-102, 24], zoom: 4.5 }">
     <SisdaiCapaXyz />
 
-    <sisdai-capa-geojson
+    <!--SisdaiCapaGeojson
+      :datos="centEdos"
+      :zIndex="2"
+      :globoInformativo="f => `Estdo: ${f.nom_edo}`"
+    /-->
+
+    <SisdaiCapaGeojson
       :datos="edos"
       :zIndex="1"
-      :globoInformativo="f => `Estdo: ${f.nom_edo}`"
+      :globoInformativo="contenidoGloboInformativo"
     />
   </SisdaiMapa>
 </template>
